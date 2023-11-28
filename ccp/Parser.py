9e19dc13a7bd679f02ac7b9e7fda2ad14409923f
bruno2023sa <119510986+bruno2023sa@users.noreply.github.com>
2023-11-28 03:12:36 -0300

    Update Parser.py
    adicionando comentários

--- ccp/Parser.py
+++ ccp/Parser.py
@@ -1,7 +1,11 @@
 from Command import Command
 class Parser:
     def __init__(self, input_text):
+        # Cria uma lista de objetos Command a partir das linhas não vazias do texto de entrada
+
         self.commands = [Command(line.split()) for line in input_text.split('\n') if line.strip()]
+        # Cria uma lista de tokens a partir das linhas do texto de entrada, excluindo comentários e linhas vazias
+        
         self.tokens = [line.split() for line in input_text.split('\n') if "//" not in line and line.strip() != ""]
 
     def command(self):
